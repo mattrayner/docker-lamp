a6347a2f278ddc61c3df179e06926eceeddd1aa3
Mozi <29089388+pzhlkj6612@users.noreply.github.com>
2019-06-15 21:28:44 +0800

    Fix path to 7.3 php.ini

--- supporting_files/run.sh
+++ supporting_files/run.sh
@@ -8,7 +8,7 @@ then
         -e "s/^post_max_size.*/post_max_size = ${PHP_POST_MAX_SIZE}/" /etc/php/5.6/apache2/php.ini
 else
     sed -ri -e "s/^upload_max_filesize.*/upload_max_filesize = ${PHP_UPLOAD_MAX_FILESIZE}/" \
-        -e "s/^post_max_size.*/post_max_size = ${PHP_POST_MAX_SIZE}/" /etc/php/7.2/apache2/php.ini
+        -e "s/^post_max_size.*/post_max_size = ${PHP_POST_MAX_SIZE}/" /etc/php/7.3/apache2/php.ini
 fi
 
 
