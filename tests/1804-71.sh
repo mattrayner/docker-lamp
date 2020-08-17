20d89de6f3c9f79f65409784dc4e9b9816ed67bf
Christian Nyffenegger <nyffenegger@simpit.ch>
2020-08-17 11:25:10 +0200

    working testing for 7.1

--- tests/1804-71.sh
+++ tests/1804-71.sh
@@ -4,5 +4,5 @@ source _helpers.sh
 waitForSupervisordProcess web1804-php71 apache2
 waitForSupervisordProcess web1804-php71 mysqld
 
-testimage 1804-php71 3000
-testmysql web1804-php71 3001
+testimage 1804-php71 3020
+testmysql web1804-php71 3021
